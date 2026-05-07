f5487b44370c7ee5eb6017c36e0435234a9a734f
Watanabe Konosuke <258505@st.takushoku-u.ac.jp>
2026-05-07 17:33:43 +0900

    課題

--- practice-js/hello.js
+++ practice-js/hello.js
@@ -1,1 +1,7 @@
 console.log('こんにちは');
+
+let x;
+x = 3;
+x = x + 1;
+
+console.log(x);
--- practice-js/obj-practice.js
+++ practice-js/obj-practice.js
@@ -16,3 +16,12 @@ let gakka = [
 
 //////////////// ここから下にプログラムを書きたそう!
 
+console.log(campus.address); 
+
+for (let n of campus.buildingD){
+  console.log(n);
+}
+
+for (let n of gakka){
+  console.log(n.name);
+}
